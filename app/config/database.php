19b23b887e87a8d6d660b31565531b2a60f88de0
Albin Norblad <walt_norblad@spawn.dreamhost.com>
2011-09-25 14:45:23 -0700

    udpated passwords and settings

--- app/config/database.php
+++ app/config/database.php
@@ -9,20 +9,20 @@ class DATABASE_CONFIG {
 		'password' => '',
 		'database' => 'test_cake',
 	);
-	var $mitzvah = array(
-		'driver' => 'mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'mitzvah2010',
-		'password' => 'g0tMitzvah?',
-		'database' => 'mitzvah',
-	);
 	var $default = array(
 		'driver' => 'mysql',
 		'persistent' => false,
+		'host' => 'mysql.mitzvah-day.org',
+		'login' => 'mitzvah',
+		'password' => '',
+		'database' => 'mitzday',
+	);
+	var $oldefault = array(
+		'driver' => 'mysql',
+		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'mitzvah2010',
-		'password' => 'g0tMitzvah?',
+		'password' => '',
 		'database' => 'mitzvah',
 	);
 }
